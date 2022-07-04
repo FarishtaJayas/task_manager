310e85cfa119ff2f01a72c436bf0419ff5cc45f2
FarishtaJayas <farishtajayas@gmail.com>
2022-07-04 15:09:19 +0600

    task_manager

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button 
+    :style="{ background:color }" 
+    :click="onClick()"
+    class="btn">{{text}}</button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            text: String,
+            color: String
+        },
+
+        methods: {
+            onClick() {
+                console.log('Click')
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
+
